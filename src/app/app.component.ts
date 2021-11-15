476621543dbbeeb37a1a8039f767d59d6973c214
Zoe <zoe.shi@slalom.com>
2021-11-15 17:37:07 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  paymentDate: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onNameChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.name = value;
+  }
+
+  onDateChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.paymentDate = value;
+  }
+
+  onAmountChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.height = parseFloat(value);
+  } 
+  
+    onMilesChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.miles = parseFloat(value);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    if (!value) {
+      return '';
+    }
+    return <number>value * 1.60934;
+  }
+
+}
